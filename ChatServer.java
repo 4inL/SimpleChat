76357e16b13444fdeb8ebeede6229c45be4ed483
4inL <ms029369@naver.com>
2019-05-27 16:50:28 +0900

    5.27

--- ChatServer.java
+++ ChatServer.java
@@ -1,3 +1,7 @@
+// My Github Address : https://github.com/4inL/SimpleChat
+// Writer : 21400523 LEE MYEONG SEOK
+// Date   : 2019-05-27
+
 import java.net.*;
 import java.io.*;
 import java.util.*;
@@ -25,7 +29,8 @@ class ChatThread extends Thread{
 	private String id;
 	private BufferedReader br;
 	private HashMap hm;
-	private boolean initFlag = false;
+	private boolean initFlag = false;i
+	private String[] badwords = {"Fuck", "fuck"};		
 	public ChatThread(Socket sock, HashMap hm){
 		this.sock = sock;
 		this.hm = hm;
@@ -45,14 +50,31 @@ class ChatThread extends Thread{
 	} // construcor
 	public void run(){
 		try{
+			int bad = 0;
+			Object obj = hm.get(id);
+			PrintWriter pw = (PrintWriter)obj;
 			String line = null;
 			while((line = br.readLine()) != null){
+				for(int i = 0; i < badwords.length; i++) {	  		//////
+					bad = 0;
+					if( line.contains(badwords[i]) ) {
+						pw.println("YOU CANNOT SAY IT !!!");
+						pw.flush();
+						bad = bad+1;
+						break;
+					}
+				}	//check bad words
+
 				if(line.equals("/quit"))
 					break;
-				if(line.indexOf("/to ") == 0){
+				if(bad != 0){
+					continue;
+				}else if(line.equals("/userlist")) {
+					send_userlist(id);
+				}else if(line.indexOf("/to ") == 0){
 					sendmsg(line);
 				}else
-					broadcast(id + " : " + line);
+					broadcastexcept(id + " : " + line, pw);
 			}
 		}catch(Exception ex){
 			System.out.println(ex);
@@ -92,4 +114,32 @@ class ChatThread extends Thread{
 			}
 		}
 	} // broadcast
+	public void broadcastexcept(String msg, PrintWriter hostpw){ 		//////
+		synchronized(hm){
+			Collection collection = hm.values();
+			Iterator iter = collection.iterator();
+			while(iter.hasNext()){
+				PrintWriter pw = (PrintWriter)iter.next();
+				if(pw.equals(hostpw)) {
+				} else{
+					pw.println(msg);
+					pw.flush();
+				}
+			}
+		}
+	} // broadcast except himself
+	public void send_userlist(String id) {
+		Object obj = hm.get(id);
+		if(obj != null){
+		  PrintWriter pw = (PrintWriter)obj;
+			synchronized(hm){
+				Collection collection = hm.keySet();
+				Iterator iter = collection.iterator();
+				while(iter.hasNext()){
+					pw.println(iter.next());
+					pw.flush();
+				}
+			}
+		}
+	}	//print userlist
 }
